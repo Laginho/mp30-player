e17005e8075f570e65f053e328e9fd46ac532ea8
Your Name <raulfalcao17@gmail.com>
2026-01-08 13:03:44 -0300

    Adicionar métodos para criar slider de tempo e botões customizados

--- src/main/java/br/ufc/poo/visao/ComponentesCustomizados.java
+++ src/main/java/br/ufc/poo/visao/ComponentesCustomizados.java
@@ -1,5 +1,21 @@
 package br.ufc.poo.visao;
 
-public class ComponentesCustomizados {
+import javax.swing.JButton;
+import javax.swing.JSlider;
 
+public class ComponentesCustomizados {
+    
+    //Nessa classe, vamos adicionar componentes para "encorpoar" o MP3
+    
+    //Criando um slider que mostra o tempo da música
+    public static JSlider criarSliderTempo() {
+        JSlider slider = new JSlider(0, 100, 0);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(false);
+        return slider;
+    }
+  public static JButton criarBotao(String texto) {
+        return new JButton(texto);
+    // Caso a gente queira, futuramente, esse método permite criar botões customizados
+    } 
 }
